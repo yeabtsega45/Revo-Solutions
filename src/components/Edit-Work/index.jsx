7531177eb972a077697e41b90ed8a798fce79d05
yeabtsega45 <yabtsega2022@gmail.com>
2025-02-24 10:23:18 +0300

    update edit work page

--- src/components/Edit-Work/index.jsx
+++ src/components/Edit-Work/index.jsx
@@ -1,33 +1,54 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Link from "next/link";
-import initIsotope from "../../common/initIsotope";
 import { works } from "@/src/data/works";
+import axios from "axios";
 
 const EditWork = () => {
-  React.useEffect(() => {
-    setTimeout(() => {
-      if (window.Isotope) initIsotope();
-    }, 1000);
-  }, []);
+  // delete work
+  const handleDelete = (id) => {
+    axios
+      .delete("/work/delete/" + id, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => {
+        window.location.reload(true);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
       <section className="works filter-img three-col section-padding">
         <div className="container">
+          <h2 className="title">Edit Works</h2>
           <div className="row gallery">
             {works.map((work, index) => (
-              <Link href={`/admin/${work.id}`} key={index} passHref>
-                <div className={`col-lg-4 col-md-6 items ${work.category}`}>
+              <Link href={`/admin/edit/${work.id}`} key={index} passHref>
+                <div className="col-lg-4 col-md-6 items">
                   <div className="item">
                     <div className="img">
                       <img src={work.imgSrc} alt={work.title} />
                     </div>
                     <div className="cont">
                       <h5>{work.title}</h5>
-                      {work.tags.map((tag, index) => (
+                      {work.category.map((tag, index) => (
                         <span key={index}>{tag}</span>
                       ))}
                     </div>
+                  </div>
+                  <div className="buttons">
+                    <Link href={`/admin/edit/` + work.id} passHref>
+                      <button className="btn btn-sm me-2">edit</button>
+                    </Link>
+                    <button
+                      onClick={() => handleDelete(work.id)}
+                      className="btn btn-sm btn-danger"
+                    >
+                      delete
+                    </button>
                   </div>
                 </div>
               </Link>
---
+++ src/pages/admin/edit/[id].jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import WorkForm2 from "@/src/components/Work-Form/Form2";
+import AdminLayout from "@/src/layouts/admin-layout";
+
+function EditWork() {
+  return (
+    <AdminLayout>
+      <WorkForm2 />
+    </AdminLayout>
+  );
+}
+
+export default EditWork;
